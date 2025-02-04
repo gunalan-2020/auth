53d817856b2435f1fdb5347f60cc0e469ead1f1a
gunalan <gunalandeedzooy@gmail.com>
2025-02-04 23:54:58 +0530

    change the jwt cookies store backend to frontend

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get(ConfigService);
   const frontendUrl = configService.get<string>('FRONTEND_URL');
 
+  app.set('trust proxy', 1);
   app.use(cookieParser());
   app.enableCors({
     origin: frontendUrl, // Adjust based on frontend URL
